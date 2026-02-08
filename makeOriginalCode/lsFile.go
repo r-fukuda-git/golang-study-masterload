ec0ed493c8e2d444bdbcb0aee0c267dc1ed96ac6
r-fukuda <futianluren195@gmail.com>
2026-02-08 21:11:00 +0900

    20260208

--- makeOriginalCode/lsFile.go
+++ makeOriginalCode/lsFile.go
@@ -31,64 +31,77 @@ func lsFile() {
 	w := tabwriter.NewWriter(os.Stdout, 0, 8, 2, ' ', 0) // 最終的な出力するための変数で定義
 
 	// list := flag.Bool("-l", false, "詳細に表示します") // 引数を確認するため
-	list := flag.Bool("l", false, "詳細に表示")
+	list := flag.Bool("l", false, "詳細に表示") // ハイフンは含めないのが一般的
+	// flag.Bool という関数の「戻り値の型」が、最初から *bool（ポインタ型）として設計されている
 	flag.Parse()
 
-	// fileName, err := os.ReadDir(dir) // ←この状態だとカレントディレクトリの情報を取得してしまっているため、ループで一つずつのファイルを処理する必要がある
-	fileName, err := os.ReadDir(dir)
-	for _, fileNames := range fileName {
-		// for ..rangeについて2つの値を返してきます。
-		// 第1戻り値: 現在の要素の インデックス（添え字）。0, 1, 2... という数字。
-		// 第2戻り値: 現在の要素の 中身（値）。今回で言えば、ファイル情報そのもの。
-		// go wayのため、変数したルールは必ず使用しないといけないので、_,として変数をブランクにおいている
-		nilProcess(err)
-		// fmt.Println(fileNames)
+	if *list {
+		//このlistがなぜポインタ付きなのかが謎？？
+		// ① 「値（あたい）」そのものを入れる変数
+		// ② 「ポインタ（住所）」を入れる変数
+		// bool 型: true か false かを判定できる。
+		// *bool 型（ポインタ）: 0x14000... という 「数字（住所）」。
+		// fileName, err := os.ReadDir(dir) // ←この状態だとカレントディレクトリの情報を取得してしまっているため、ループで一つずつのファイルを処理する必要がある
+		fileName, err := os.ReadDir(dir)
+		for _, fileNames := range fileName {
+			// for ..rangeについて2つの値を返してきます。
+			// 第1戻り値: 現在の要素の インデックス（添え字）。0, 1, 2... という数字。
+			// 第2戻り値: 現在の要素の 中身（値）。今回で言えば、ファイル情報そのもの。
+			// go wayのため、変数したルールは必ず使用しないといけないので、_,として変数をブランクにおいている
+			nilProcess(err)
+			// fmt.Println(fileNames)
 
-		// ここで1つのファイル情報を取得しているため
-		// 詳細を出力させる
-		// fileSize, err := os.Stat(dir)
-		// 上記の形だと現在のフォルダそのものを持ってきている状態なので、フォルダ+ファイルを組み合わせないといけない
-		fullPath := filepath.Join(dir, fileNames.Name())
-		fileInfo, err := os.Stat(fullPath)
-		nilProcess(err)
-		// fmt.Println(fileInfo)
+			// ここで1つのファイル情報を取得しているため
+			// 詳細を出力させる
+			// fileSize, err := os.Stat(dir)
+			// 上記の形だと現在のフォルダそのものを持ってきている状態なので、フォルダ+ファイルを組み合わせないといけない
+			fullPath := filepath.Join(dir, fileNames.Name())
+			fileInfo, err := os.Stat(fullPath)
+			nilProcess(err)
+			// fmt.Println(fileInfo)
 
-		//	list := flag.Bool("-l", false, "詳細に表示します")
-		//	flag.Parse()
-		// これはループの中で呼びだす必要はない
+			//	list := flag.Bool("-l", false, "詳細に表示します")
+			//	flag.Parse()
+			// これはループの中で呼びだす必要はない
 
-		if len(list) == 0 {
-			// 引数がないときは名前表示のみ
+			// 詳細に表示できたので、ここから変えていく
+			// userId, err := user.LookupId(dir)
+			// nilProcess(err)
+			// fmt.Println(userId)
+			// 上記ではUIDを取り出せずにエラーになってしまう
+			stat := fileInfo.Sys().(*syscall.Stat_t)
+			// fileInfo.Size(): 全OS共通（表のメニュー）であり、fileInfo.Sys(): 各OS独自の生データ（裏メニューへの入り口）になる
+			// 基本的にGolangは他OSでも動くようにできているけど、UIDやGIDはUNIXのみの機能のため、Sys()という設定が必要になってくる
+			// fileInfo.Sys()の中身はなんでも入るany型となっているので、Mac/Linuxの統計データ（*syscall.Stat_t）という中身を断定しているアサーション設定が必要になってく
+
+			uid := fmt.Sprint(stat.Uid)
+			// uid := fmt.Printf(stat.Uid)
+			// fmt.Printfについてf画面に出力するという副作用がメインの関数です。戻り値として「文字列」を返してくれません。
+			// fmt.Sprint渡されたデータを文字列に変えて 戻り値 として返すための関数なので、今回戻り値が必要なSprintを使用する
+
+			u, err := user.LookupId(uid)
+			// fmt.Println(u)
+
+			modTime := fileInfo.ModTime().Format("Jan _2 15:04") // タイムスタンプ用の設定
+			fmt.Fprintln(w, fileInfo.Mode(), fileInfo.Size(), u.Username, modTime, fileNames.Name())
 		}
-		// 詳細に表示できたので、ここから変えていく
-		// userId, err := user.LookupId(dir)
-		// nilProcess(err)
-		// fmt.Println(userId)
-		// 上記ではUIDを取り出せずにエラーになってしまう
-		stat := fileInfo.Sys().(*syscall.Stat_t)
-		// fileInfo.Size(): 全OS共通（表のメニュー）であり、fileInfo.Sys(): 各OS独自の生データ（裏メニューへの入り口）になる
-		// 基本的にGolangは他OSでも動くようにできているけど、UIDやGIDはUNIXのみの機能のため、Sys()という設定が必要になってくる
-		// fileInfo.Sys()の中身はなんでも入るany型となっているので、Mac/Linuxの統計データ（*syscall.Stat_t）という中身を断定しているアサーション設定が必要になってく
+		w.Flush()
 
-		uid := fmt.Sprint(stat.Uid)
-		// uid := fmt.Printf(stat.Uid)
-		// fmt.Printfについてf画面に出力するという副作用がメインの関数です。戻り値として「文字列」を返してくれません。
-		// fmt.Sprint渡されたデータを文字列に変えて 戻り値 として返すための関数なので、今回戻り値が必要なSprintを使用する
+		// nilProcess(err) //上記にループ文に加える
+		// fmt.Println(fileName) //上記にループ文に加える
 
-		u, err := user.LookupId(uid)
-		// fmt.Println(u)
+		// fileSize, err := os.Stat(dir) //上記にループ文に加える
+		// nilProcess(err) //上記にループ文に加える
+		// fmt.Println(fileSize) //上記にループ文に加える
+	} else {
+		fileName, err := os.ReadDir(dir)
+		for _, fileNames := range fileName {
+			fmt.Println(fileNames)
+			nilProcess(err)
+		}
 
-		modTime := fileInfo.ModTime().Format("Jan _2 15:04") // タイムスタンプ用の設定
-		fmt.Fprintln(w, fileInfo.Mode(), fileInfo.Size(), u.Username, modTime, fileNames.Name())
 	}
-	w.Flush()
 
-	// nilProcess(err) //上記にループ文に加える
-	// fmt.Println(fileName) //上記にループ文に加える
-
-	// fileSize, err := os.Stat(dir) //上記にループ文に加える
-	// nilProcess(err) //上記にループ文に加える
-	// fmt.Println(fileSize) //上記にループ文に加える
 }
 
 //	ヒント
